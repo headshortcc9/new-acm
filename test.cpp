7b9f4dfa789c823ab790f8739727b5f16ceccfd7
nghianguyen <nghianx.t94@gmail.com>
2023-07-11 21:56:17 +0700

    sync

--- test.cpp
+++ test.cpp
@@ -1,98 +1,62 @@
-#include <iostream>
-#include <regex>
-#include <sstream>
-#include <string>
-#include <string_view>
+#include<iostream>
+#include <fstream>
 #include <vector>
 
-#include "src/lib/utility.h"
+using namespace std;
 
-void PrintErrorMessage() {
-  std::cout << "Error: command line arguments were not correct." << std::endl;
-  std::cout << "Example usage: translate my_file.txt japanese 2 true"
-            << std::endl;
-}
-
-int ProcessCommandLine(int argc, const char* argv[]) {
-  if (argc < 2) {
-    PrintErrorMessage();
-    return -1;
-  }
-
-  std::string input = "default_file.txt";
-  std::string from = "japanese";
-  int accuracy = 2;
-  bool overwrite = false;
-
-  // Regular expression matching --parameter=value.
-  // Example: --input=my_file.txt
-  // myprogram --input=my_file.txt --timeout=5
-  // myprogram --timeout=5 --input=my_file.txt
-
-
-  std::regex pattern("--(.*)=(.*)");
-
-  // Iterate each element of argc
-  try {
-    for (size_t i = 1; i < argc; i++) {
-      std::string param_to_search = argv[i];
-      std::smatch matches;
-      if (std::regex_match(param_to_search, matches, pattern)) {
-        auto it = matches.begin();
-        // The first match because it's the entire string.
-        it++;
-
-        // The second match is the parameter name.
-        if ((*it) == "input") {
-          // The third match is the parameter value.
-          it++;
-          input = *it;
-          continue;
-        }
-
-        // The second match is the parameter name.
-        if ((*it) == "from") {
-          // The third match is the parameter value
-          it++;
-          from = *it;
-          continue;
-        }
-
-        if ((*it) == "accuracy") {
-          it++;
-          accuracy = std::stoi(*it);
-          continue;
-        }
-
-        if ((*it) == "overwrite") {
-          it++;
-          std::istringstream((*it)) >> std::boolalpha >> overwrite;
-          continue;
-        }
-
-        PrintErrorMessage();
-        return -1;
-
-      } else {
-        PrintErrorMessage();
-        return -1;
-      }
+void print(std::vector<string> const &input)
+{
+    for (int i = 0; i < input.size(); i++) {
+        std::cout << input.at(i) << ' ';
     }
-  } catch (const std::exception& e) {
-    std::cout << "Exception!" << std::endl;
-    PrintErrorMessage();
-    return -1;
-  }
-  std::cout << "input: " << input << std::endl;
-  std::cout << "from: " << from << std::endl;
-  std::cout << "accuracy: " << accuracy << std::endl;
-  std::cout << "overwrite: " << std::boolalpha << overwrite << std::endl;
-
-  return 0;
 }
-
-int main() {
-  const char* argv[] = {"test", "--input=my_file.txt", "--from=Japanese",
-                        "--accuracy=a", "--overwrite=true"};
-  return ProcessCommandLine(5, argv);
+void read_file() {
+    ifstream MyReadFile("db/db.txt");
+    string s;
+    string group = "argo";
+    string identifier = "stg";
+    try
+    {
+      int status = 0;
+      while (getline (MyReadFile, s)) 
+      {
+        size_t pos = 0;
+        std::string token;
+        pos = s.find(":");
+        if(s[s.length()-1] != ':' || s[0] == ':') throw 101;
+        if(s.substr(0, pos) == group){
+          vector <string> items;
+          while ((pos = s.find(":")) != std::string::npos) {
+            items.push_back(s.substr(0, pos));
+            s.erase(0, pos + 1);
+          }
+          if(items[1] == identifier){
+            if(items.size() < 2 || items.size() > 4){
+              throw 103;
+            }
+            else{
+              if(items.size() == 3){
+                cout<<"Token: "<<items[2]<<endl;
+              }
+              else if(items.size() == 4){
+                cout<<"Username: "<<items[2]<<endl;
+                cout<<"Password: "<<items[3]<<endl;
+              }
+            }
+            break;
+          }
+          status++;
+          cout<<endl;
+        }
+      }
+      if(status == 0) throw 102;
+    }
+    catch(int e){
+        cout<<e<<endl;
+    }
+      //cout << myText << endl;
+}
+int main(){
+  read_file();
+  return 0;
 }
